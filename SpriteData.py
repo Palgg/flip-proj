83696e925e280fc4523695116a58c03f87b561fc
Stephen Palagi <spalagi@umass.edu>
2016-11-23 11:25:29 -0500

    made sprite loading generic

--- SpriteData.py
+++ SpriteData.py
@@ -5,6 +5,12 @@
 
 import pygame
 
+# returns a sprite at the given location
+def load_sprite(sheet, x_loc, y_loc, h, w):
+	sheet.set_clip(pygame.Rect(x_loc, y_loc, h, w))
+	sprite = sheet.subsurface(sheet.get_clip())
+	return sprite
+
 """
 	SPRITESHEETS
 """
@@ -21,21 +27,13 @@ arena_level = pygame.image.load("res/maps/arena_1.png")
 	SPRITES TO LOAD ONCE
 """
 # --- fighter ---
-char_sheet.set_clip(pygame.Rect(160, 128, 32, 32))
-fighter = char_sheet.subsurface(char_sheet.get_clip())
+fighter = load_sprite(char_sheet, 160, 128, 32, 32)
 # --- arcanist ---
-char_sheet.set_clip(pygame.Rect(0, 128, 32, 32))
-arcanist = char_sheet.subsurface(char_sheet.get_clip())
+arcanist = load_sprite(char_sheet, 0, 128, 32, 32)
 # --- pathfinder ---
-char_sheet.set_clip(pygame.Rect(448, 96, 32, 32))
-pathfinder = char_sheet.subsurface(char_sheet.get_clip())
+pathfinder = load_sprite(char_sheet, 448, 96, 32, 32)
 
 # --- skeleton ---
-char_sheet.set_clip(pygame.Rect(577, 512, 32, 32))
-skeleton = char_sheet.subsurface(char_sheet.get_clip())
+skeleton = load_sprite(char_sheet, 577, 512, 32, 32)
 
 
-# returns a sprite at the given location
-def load_sprite(x_loc, y_loc, h, w):
-	char_sheet.set_clip(pygame.Rect(x_loc, y_loc, h, w))
-	sprite = char_sheet.subsurface
